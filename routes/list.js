589b995079a718a522ee4d4d385f2b8d09c74433
parkhidevtown <parkhigarg@devtown.in>
2024-05-18 13:14:22 +0530

    update

--- routes/list.js
+++ routes/list.js
@@ -2,7 +2,24 @@ const express = require("express");
 const router = express.Router();
 const listController = require("../controllers/list");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const fs = require("fs");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadDir = "../config/uploads";
+
+    fs.mkdir(uploadDir, { recursive: true }, function (err) {
+      if (err) {
+        console.error("Error creating upload directory:", err);
+      }
+      cb(null, uploadDir);
+    });
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 /*
 Description - router to create List
